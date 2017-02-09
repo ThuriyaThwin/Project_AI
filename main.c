f78302fcb8e44286e7d51bda4d3ec1bb81b7908a
spoken <spoken.spok@gmail.com>
2017-02-09 17:34:56 +0100

    fini

--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 #include "generator.h"
 
 int main(int argc, char* argv[]){
+
+    if(argc != 2){
+        fprintf(stderr, "Nombre d arguments invalide\n");
+        return 1;
+    }
+
+
     pigeonsGenerator( atoi(argv[1]) );
     return 0;
 }
