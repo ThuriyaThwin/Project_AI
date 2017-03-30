17f6d2566f5737fd9e6462a949f3d834902e70e9
spoken <spoken.spok@gmail.com>
2017-03-30 16:31:07 +0200

    ajout modifications

---
+++ backjumping.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int** copyMatrice(int** matriceSource, int nbPigeons){
+    int** newMatrice;
+
+    newMatrice = malloc(nbPigeons * sizeof(int*));
+    for(int i = 0; i < nbPigeons; ++i){
+        newMatrice[i] = malloc( (nbPigeons - 1) * sizeof(int) );
+    }
+
+    for(int i=0; i < nbPigeons; ++i){
+        for(int j=0; j < (nbPigeons-1); ++j){
+            newMatrice[i][j] = matriceSource[i][j];
+        }
+    }
+
+    return newMatrice;
+}
+
+int main(int argc, char** argv){
+    if( argc != 2 ){
+        printf("erreur arguments\n");
+        exit(-1);
+    }
+
+    int i, j;
+    int **domaines;
+    int nbPigeons = atoi(argv[1]);
+
+    printf("Démarrage avec %d pigeons & %d nids.\n", nbPigeons, (nbPigeons-1));
+    printf("MATRICE DOMAINE :\n");
+
+
+    domaines = malloc(nbPigeons * sizeof(int*));
+    for(i = 0; i < nbPigeons; ++i){
+        domaines[i] = malloc( (nbPigeons - 1) * sizeof(int) );
+    }
+
+    for(i = 0; i < nbPigeons; ++i){
+        for(j = 0; j < nbPigeons - 1; ++j){
+            domaines[i][j] = 0;
+            printf("%d", domaines[i][j]);
+        }
+        printf("\n");
+    }
+
+    int** domaines_init = copyMatrice(domaines, nbPigeons);
+    printf("%d\n", domaines_init[0][0]);
+
+    for(i=0; i < nbPigeons; ++i){ free(domaines[i]); }
+    free( domaines );
+
+    return 0;
+}
